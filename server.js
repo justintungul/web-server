9d9b66d7b03265ab7b5cbe69ec09a5681c17bc0b
Justin Tungul <justin.tungul@gmail.com>
2017-06-16 10:52:30 +0800

    Added middleware.js module

--- server.js
+++ server.js
@@ -2,20 +2,13 @@ var express = require('express');
 var app = express();
 var PORT = 3000; //uppercase denotes a CONSTANT varialbe
 
-var middleware = {
-	requireAuthentication: function (req, res, next) {
-		console.log('private route hit!');
-		next();
-	},
-	logger: function (req, res, next) {
-		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-		next();
-	}
-}
+var middleware = require('./middleware.js');
 
+// Application-Level-Middleware
 app.use(middleware.logger);
 
-app.get('/about', function(req, res) {
+// Route-Level-Middleware
+app.get('/about', middleware.requireAuthentication, function(req, res) {
 	res.send('About Us!!!');
 });
 
